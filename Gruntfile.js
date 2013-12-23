61e54a641709d99e77dd6be22648e3b74c56483c
Hugo Bessa <hugobessaa@gmail.com>
2013-12-23 05:03:03 -0200

    New build directives

--- Gruntfile.js
+++ Gruntfile.js
@@ -268,11 +268,12 @@ module.exports = function (grunt) {
             'img/**/*',
             'fonts/**/*',
             // Like Jekyll, exclude files & folders prefixed with an underscore
-            '!**/_*{,/**}'
+            '!**/_*{,/**}',
             // Explicitly add any files your site needs for distribution here
             //'_bower_components/jquery/jquery.js',
             //'favicon.ico',
             //'apple-touch*.png'
+            'CNAME'
           ],
           dest: '<%= yeoman.dist %>'
         }]
