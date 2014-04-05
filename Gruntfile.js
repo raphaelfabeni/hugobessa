edd1047928d27148b3feb935ee31578e55366b41
Hugo Bessa <hugobessaa@gmail.com>
2014-04-05 00:49:59 -0300

    No CoffeeScript anymore. Just a cup of coffee please

---
+++ Gruntfile.js
@@ -0,0 +1,177 @@
+'use-strict';
+module.exports = function(grunt) {
+
+    // Load'em all!
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+
+        // Project paths
+        paths: {
+            app:    'app/',
+            assets: '_assets/',
+            build: { 
+                dev: '.dev/',
+                prod: 'dist/'
+            },
+            fonts:  'fonts/',
+            sass:   '_scss/',
+            img:    'images/',
+            css:    'css/',
+            js:     'js/'
+        },
+
+        // Watch: trigger tasks on file changes/add
+        watch: {
+            options: {
+                nospawn: true,
+                livereload: false,
+            },
+            sass: {
+                files: ['<%= paths.app %><%= paths.assets %><%= paths.sass %>**/*.{scss,sass}'],
+                tasks: ['sass:dev']
+            },
+            jekyll: {
+                files: '<%= paths.app %>**/*.{html,md}',
+                tasks: ['buildDev']
+            }
+        },
+
+        // Browser-sync: sync navigation and file changes
+        browser_sync: {
+            options: {
+                watchTask: true,
+                reloadDelay: 1000,
+
+                ghostMode: {
+                    scroll: true,
+                    links: true,
+                    forms: true,
+                    clicks: true
+                }
+            },
+            dev: {
+                bsFiles: {
+                    src: [
+                        '<%= paths.build.dev %><%= paths.assets %><%= paths.css %>**/*.css',
+                        '<%= paths.build.dev %><%= paths.assets %><%= paths.img %>**/*.{png,jpg,gif}',
+                        '<%= paths.build.dev %><%= paths.assets %><%= paths.js %>**/*.js',
+                        '<%= paths.build.dev %>**/*.html'
+                    ]
+                },
+                options: {
+                    server: {
+                        baseDir: '<%= paths.build.dev %>'
+                    }
+                }
+            },
+            prod:{
+                bsFiles: {
+                    src: [
+                        '<%= paths.build.prod %><%= paths.css %>**/*.css',
+                        '<%= paths.build.prod %><%= paths.img %>**/*.{png,jpg,gif}',
+                        '<%= paths.build.prod %><%= paths.js %>**/*.js',
+                        '<%= paths.build.prod %>**/*.html'
+                    ],
+                },
+                options: {
+                    server: {
+                        baseDir: '<%= paths.build.prod %>'
+                    }
+                }
+            }
+        },
+
+        // Sass: compiles SCSS files
+        sass: {
+            dev: {
+                cwd: '<%= paths.app %><%= paths.assets %><%= paths.sass %>',
+                src: '**/*.scss',
+                ext: '.css',
+                expand: true,
+                dest: '<%= paths.build.dev %><%= paths.assets %><%= paths.css %>'
+            },
+            prod: {
+                cwd: '<%= paths.app %><%= paths.assets %><%= paths.sass %>',
+                src: '**/*.scss',
+                ext: '.css',
+                expand: true,
+                dest: '<%= paths.build.prod %><%= paths.assets %><%= paths.css %>'
+            }
+        },
+
+        // Jekyll: generates the blog
+        jekyll: {
+            options: {
+                src: '<%= paths.app %>',
+                config: ['_config.yml', '_config.build.yml'],
+                bundleExec: true,
+                keep_files: ['_assets/*']
+            },
+            dev: {
+                options: {
+                    dest: '<%= paths.build.dev %>',
+                    config: '_config.yml'
+                    // drafts: true,
+                }
+            },
+            prod: {
+                options: {
+                    dest: '<%= paths.build.prod',
+                    config: ['_config.yml', '_config.build.yml']
+                }
+            }
+        },
+
+        // Imagemin: optimizes images
+        imagemin: {
+            prod: {
+                options: {
+                    optimizationLevel: 7,
+                    progressive: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= paths.app %><%= paths.assets %><%= paths.img %>',
+                    src: '**/*.{png,jpg,gif}',
+                    dest: '<%= paths.build.prod %><%= paths.img %>'
+                }]
+            }
+        },
+
+        // Clean: remove files from folder
+        clean: {
+            dev: ['<%= paths.build.dev %>'],
+            prod: ['<%= paths.build.prod %>']
+        },
+
+        // Copy: copy files from a folder to another folder
+        copy: {
+            dev: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= paths.app %><%= paths.assets %><%= paths.img %>',
+                    src: ['**'],
+                    dest: '<%= paths.build.dev %><%= paths.assets %><%= paths.img %>'
+                }]
+            }
+        }
+    });
+
+    // complex tasks
+    grunt.registerTask('buildDev',[
+        'clean:dev',
+        'jekyll:dev',
+        'sass:dev',
+        'copy:dev'
+    ]);
+
+    grunt.registerTask('serve', [
+        'buildDev',
+        'browser_sync:dev',
+        'watch'
+    ]);
+
+    // default task
+    grunt.registerTask('default', ['browser_sync', 'watch']);
+};
